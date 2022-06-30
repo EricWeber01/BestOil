8458b7582f166370252b5707d93c840c1a753651
babya <babya@ERIC_WEBER>
2022-06-30 12:45:05 +0300

    Add project files.

---
+++ BestOil/Cafe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestOil
+{
+    public class Cafe
+    {
+        public double HotdogPrice { get; set; }
+        public double HamburgerPrice { get; set; }
+        public double KartofPrice { get; set; }
+        public double ColaPrice { get; set; }
+    }
+}
